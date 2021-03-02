7bf25c898aaf6bf0af64f2dcd663835e50ed8d77
michiburro14@gmail.com <michiburro14@gmail.com>
2021-03-02 21:40:33 +0100

    Add project files.

---
+++ FrequenzaArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using libreria;
+
+namespace FrequenzaArray
+{
+    class Program
+    {
+        const int NUM = 10;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Inserisci 10 valori: ");
+            int x = int.Parse(Console.ReadLine());
+            int[] arrayInteri = new int[x];
+            Gestione.RiempiArray(arrayInteri);
+            Gestione.BubbleSort(arrayInteri);
+        }                                
+    }
+}
---
+++ libreria/Gestione.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace libreria
+{
+    public class Gestione
+    {
+        public static void BubbleSort(int[] n)
+        {
+            int x, y;
+            int temp;
+            for (x = 0; x < n.Length - 1; x++)
+            {
+                for (y = 0; y < n.Length - 1 - x; y++)
+                {
+                    if (n[y] > n[y + 1])
+                    {
+                        temp = n[y];
+                        n[y] = n[y + 1];
+                        n[y + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void RiempiArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static void StampaArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
